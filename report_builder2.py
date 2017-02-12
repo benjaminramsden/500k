86eb7c959307ba513b9af4823eab2f97f6139e53
Ben Ramsden <benjaminramsden@gmail.com>
2017-02-12 17:20:07 +1100

    Create builder for webform to process all reports at once

---
+++ report_builder2.py
@@ -0,0 +1,195 @@
+from pptx import Presentation
+from docx import Document
+from imgurpython import ImgurClient
+from datetime import datetime
+import sys, os, shutil
+from utils import *
+from sheets_api import *
+
+# This script conducts the following:
+# - Gets the information on a missionary based on Miss ID (gets all)
+# - Populates the title slide with the missionaries details
+# - Creates a content slide per report
+# - Exports the slideshow to pdf
+# - Uploads to Ben's Google Drive account via API
+# - Pastes the URL of the report in Google Drive to the web report sheets
+#
+# With this info Ben can then send out multiple reports using:
+# https://support.yet-another-mail-merge.com/hc/en-us/articles/210735349
+
+def main(argv=None):
+    # Gather all information from the spreadsheet. Returned as list of lists
+    # where each list is a row of cells.
+    values = get_all_missionary_reports()
+
+    # For all the missionaries, arrange data in this structure:
+    # All
+    #  -> Missionary 1 (based on ID)
+    #      -> Report 1
+    #          -> Date, Subject, Raw, Missionary, Missionary ID etc...
+    #          -> Village 1
+    #              -> Village
+    #              -> People
+    #              -> Baptisms
+    #          -> Village 2
+    #           ...
+    #          -> Prayer Point 1
+    #          -> Prayer Point 2
+    #           ...
+    #      -> Report 2
+    #       ...
+    #  -> Missionary 2
+    #   ...
+    all_dict = dict()
+    for row in values:
+        report = dict("Date":         row[0],
+                      "Subject":      row[1],
+                      "Raw":          row[2],
+                      "Submitter":    row[3],
+                      "Missionary":   row[4],
+                      "Missionary ID":row[5],
+                      "Report":       row[39],
+                      )
+        for i,village in enumerate(row[6:3:24]):
+            if not village.isempty():
+                vill_dict = dict(
+                    "Village": row[i+6],
+                    "People":  row[i+7],
+                    "Baptisms":row[i+8],
+                )
+                report['Village '+str(i+1)] = vill_dict
+        for i,prayer in enumerate(row[40:47]):
+            if not prayer.isempty()
+                report['Prayer '+str(i+1)] = prayer
+        if row[5] in all_dict:
+            # Missionary already exists, add report to missionary dictionary
+            miss_dict = all_dict[row[5]]
+            miss_dict[row[0]] = report
+        else:
+            # New missionary, create new dictionary and add report to it.
+            all_dict[row[5]] = dict(row[0]: report)
+
+    # Time to create the presentations, loop around for every single missionary
+    # TODO - In future make sure only missionaries with new reports get
+    # generated
+    for miss_id,miss_dict in all_dict:
+        pptx = create_powerpoint(miss_id,miss_dict)
+
+        # Export to pdf
+        PPTtoPDF(pptx, pptx.split(".")[0] + ".pdf")
+
+def create_powerpoint(miss_id,miss_dict):
+    # Import presentation
+    path = "C:\Users\\br1\Dropbox\NCM\Reports, bills and Proposals\Ben Report Automation\\"
+    prs = Presentation(path + "Master Report Template.pptx")
+
+    # Access placeholders for Title slide
+    title_slide = prs.slides[0]
+
+    for shape in title_slide.placeholders:
+        print('%d %s' % (shape.placeholder_format.idx, shape.name))
+
+    # Insert Missionary Name
+    name_holder = title_slide.placeholders[0]
+    assert name_holder.has_text_frame
+    name_holder.text_frame.clear()
+    p = name_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    run.text = miss_dict[]
+
+    # Insert Missionary ID
+    miss_id_holder = title_slide.placeholders[11]
+    assert miss_id_holder.has_text_frame
+    miss_id_holder.text_frame.clear()
+    p = miss_id_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    run.text = miss_id
+
+    for k,report in miss_dict:
+        # Access placeholders for content slides
+        content_slide = prs.slides[1]
+
+        insert_bio(content_slide, report)
+
+        # Report title
+        title_holder = content_slide.placeholders[0]
+        assert title_holder.has_text_frame
+        title_holder.text_frame.clear()
+        p = title_holder.text_frame.paragraphs[0]
+        run = p.add_run()
+        run.text = "<Year> Report <round>"
+
+        # Actual report!
+        report_holder = content_slide.placeholders[14]
+        assert report_holder.has_text_frame
+        report_holder.text_frame.clear()
+        p = report_holder.text_frame.paragraphs[0]
+        run = p.add_run()
+        run.text = report
+
+        # Prayer heading
+        prayer_h_holder = content_slide.placeholders[15]
+        assert prayer_h_holder.has_text_frame
+        prayer_h_holder.text_frame.clear()
+        p = prayer_h_holder.text_frame.paragraphs[0]
+        run = p.add_run()
+        run.text = "Prayer Points"
+
+        # Prayer points body
+        prayer_b_holder = content_slide.placeholders[16]
+        assert prayer_b_holder.has_text_frame
+        prayer_b_holder.text_frame.clear()
+        p = prayer_b_holder.text_frame.paragraphs[0]
+        run = p.add_run()
+        run.text = prayer
+
+    # Save the powerpoint
+    print "Where should this report be saved? (Will use default staging area if none.)"
+    save_path = raw_input()
+    save_name = docx_path.split("\\")[-1].split(".")[0]
+    prs.save(save_path + save_name + ".pptx")
+
+def insert_bio(slide, report):
+    # Get totals for churches, baptisms and prayer
+    for k, value in report_dict:
+        # Only villages are nested dictionaries, so test on value is dictionary
+        if isinstance(value, dict):
+            pass
+
+    # Insert the name and the state of the Missionary
+    state_holder = content_slide.placeholders[2]
+    assert state_holder.has_text_frame
+    state_holder.text_frame.clear()
+    p = state_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    run.text = "State: " + state_dict[report["Missionary ID"][:2]]
+
+    name_holder = content_slide.placeholders[13]
+    assert name_holder.has_text_frame
+    name_holder.text_frame.clear()
+    p = name_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    run.text = report["Missionary"]
+
+    bio_holder = content_slide.placeholders[11]
+    assert bio_holder.has_text_frame
+    bio_holder.text_frame.clear()
+    p = bio_holder.text_frame.paragraphs[0]
+    bio_line("\n Churches: ", str(churches), p)
+    bio_line("\n Coming for Prayer: ", str(prayer_nos), p)
+    bio_line("\n Baptisms: ", str(baptisms), p)
+
+    profile_pic_holder = content_slide.placeholders[10]
+
+    # Insert profile picture, need to access Imgur database!
+    profile_pic_holder.insert_picture(img_path)
+
+    # Insert India Map based off state name
+    india_pic_holder = content_slide.placeholders[12]
+    india_pic_holder.insert_picture('C:\Users\\br1\Dropbox\NCM\Reports, ' +
+        'bills and Proposals\!Reporting Workflow\Map Images\\' +
+        state_dict[state_ab] + '.png')
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
---
+++ sheets_api.py
@@ -0,0 +1,132 @@
+# Google sheets API functions
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def get_all_missionary_reports():
+    # Log into Google and extract all column data.
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = '1AR7akf5vREy8YpROIDBb_wQxCtGbhCLOJ6GweXxYZB8'
+    rangeName = 'Extractor!A:AV'
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+    if not values:
+        print('No data found.')
+    else:
+        return values
+
+# Based off the missionary ID, retrieve all rows related to that ID in the form
+# of a dict of dicts.
+def get_ind_missionary_reports(miss_id):
+    # Validate input
+    assert(len(miss_id) == 6)
+    assert(miss_id.isalnum())
+    assert(miss_id[:2] in state_dict)
+
+# Once the report in PDF format has been generated, upload to Google Drive
+def upload_report(pdf_path):
+    """
+    Uploads report to Ben's 500k Google Drive account via API
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    drive_service = discovery.build('drive', 'v3', http=http)
+
+    file_metadata = { 'name' : pdf_path.split("\\")[-1] }
+    media = MediaFileUpload(pdf_path,
+                            mimetype='application/pdf',
+                            resumable=True)
+    file = drive_service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+
+    drive_url = 'https://www.googleapis.com/drive/v3/files/'
+    return file_url = drive_url + file.get('id')
+
+# When report is generated and uploaded to Google Drive, this will put the
+# URL of the report PDF into the spreadsheet.
+def write_url_to_sheet(url):
+    # Log into Google and extract all column data.
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = '1AR7akf5vREy8YpROIDBb_wQxCtGbhCLOJ6GweXxYZB8'
+    # Need to know range to paste based on Missionary ID
+    rangeName = 'Extractor!AW'
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+    if not values:
+        print('No data found.')
+    else:
+        return values
+
+    values = [
+        [
+            url
+        ],
+        # Additional rows ...
+    ]
+    body = {
+        'values': values
+    }
+    result = service.spreadsheets().values().update(
+        spreadsheetId=spreadsheet_id, range=range_name,
+        valueInputOption=value_input_option, body=body).execute()

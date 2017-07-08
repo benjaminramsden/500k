b25cb82505a24deea9dc3e04bcda6c4138d3219d
br1 <ben.ramsden@metaswitch.com>
2017-07-08 18:05:58 +1000

    move out powerpoint duties to new file

---
+++ powerpoint.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+from pptx import Presentation
+from datetime import datetime
+import os
+import shutil
+import re
+from utils import *
+from operator import itemgetter
+from Queue import Queue
+import threading
+import pythoncom
+import logging
+from imgur import get_image
+
+def build_report_slide(prs, missionary, report, report_split):
+    # Access placeholders for content slides
+    content_slide = prs.slides.add_slide(prs.slide_layouts[0])
+
+    # Add biography, mainly from factfile
+    success = insert_bio(content_slide, missionary, report)
+
+    # Report title - pull report round out of date
+    enter_report_title(report, content_slide)
+
+    # Actual report!
+    report_holder = content_slide.placeholders[14]
+    assert report_holder.has_text_frame
+    report_holder.text_frame.clear()
+    p = report_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    run.text = report_split.rstrip()
+
+    # Prayer heading
+    prayer_h_holder = content_slide.placeholders[15]
+    assert prayer_h_holder.has_text_frame
+    prayer_h_holder.text_frame.clear()
+    p = prayer_h_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    run.text = "Prayer Points"
+
+    # TODO - Combine prayers in dictionary together into Prayer points body
+    prayer_b_holder = content_slide.placeholders[16]
+    assert prayer_b_holder.has_text_frame
+    prayer_b_holder.text_frame.clear()
+    p = prayer_b_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    run.text = "\n".join(report.prayer_rqs)
+
+    return success
+
+
+def enter_report_title(report, slide):
+    title_holder = slide.placeholders[0]
+    assert title_holder.has_text_frame
+    title_holder.text_frame.clear()
+    p = title_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    if report.round:
+        run.text = str(report.round[1]) + " Report " + str(report.round[0])
+    else:
+        run.text = "Report"
+
+
+def insert_bio(slide, missionary, report):
+    # Insert state
+    state_holder = slide.placeholders[2]
+    assert state_holder.has_text_frame
+    state_holder.text_frame.clear()
+    p = state_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    try:
+        run.text = "State: " + missionary.state
+    except AttributeError:
+        missionary.state = validate_state(missionary.id[:2],
+                                          abbreviation=True,
+                                          convert_to_full=True)
+        run.text = "State: " + missionary.state
+
+    # Insert India Map based off state name
+    india_pic_holder = slide.placeholders[12]
+    try:
+        india_pic_holder.insert_picture('C:\Users\\br1\Dropbox\NCM\Reports' +
+                                        '\!Reporting Workflow\Map Images\\' +
+                                        missionary.state + '.png')
+    except IOError:
+        logging.error("ERROR: Missing state map for {0}, not added".format(
+            missionary.state))
+
+    # Insert Name
+    name_holder = slide.placeholders[13]
+    assert name_holder.has_text_frame
+    name_holder.text_frame.clear()
+    p = name_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    try:
+        run.text = missionary.first_name + " " + missionary.surname
+    except AttributeError:
+        run.text = missionary.surname
+
+    bio_holder = slide.placeholders[11]
+    assert bio_holder.has_text_frame
+    bio_holder.text_frame.clear()
+    p = bio_holder.text_frame.paragraphs[0]
+    # Apply numbers for churches and baptisms
+    churches = 0
+    prayer_nos = 0
+    baptisms = 0
+
+    for village in report.villages:
+        churches += 1
+        if village.attendance:
+            try:
+                prayer_nos += int(village.attendance)
+            except ValueError:
+                logging.info("Non-numerical value for attendance")
+                try:
+                    prayer_nos += int(village.attendance.split("-")[1])
+                except ValueError:
+                    logging.error("Invalid value for attendance {0}, "
+                                  "Missionary ID: {1}".format(
+                                      village.attendance,
+                                      missionary.id))
+        if village.baptisms:
+            try:
+                baptisms += int(village.baptisms)
+            except ValueError:
+                logging.error("Invalid value for baptisms {0}, "
+                              "Missionary ID: {1}".format(
+                                  village.baptisms,
+                                  missionary.id))
+
+    bio_line("\n Churches: ", str(churches), p)
+    bio_line("\n Coming for Prayer: ", str(prayer_nos), p)
+    bio_line("\n Baptisms: ", str(baptisms), p)
+
+    # Download Imgur picture, store off and add to report
+    profile_pic_holder = slide.placeholders[10]
+    try:
+        profile_pic_holder.insert_picture(get_image(missionary.pic))
+    except AttributeError:
+        logging.error("No headshot for {0}.".format(missionary.id))
+        profile_pic_holder.insert_picture(
+            "C:\Users\\br1\Dropbox\NCM\Reports\Ben Report Automation" +
+            "\headshot.png")
+
+    return
+
+
+def create_title_slide(prs, missionary):
+    # Access placeholders for Title slide
+    title_slide = prs.slides[0]
+
+    # Insert Missionary Name
+    name_holder = title_slide.placeholders[0]
+    assert name_holder.has_text_frame
+    name_holder.text_frame.clear()
+    p = name_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    try:
+        run.text = missionary.first_name + " " + missionary.surname
+    except AttributeError:
+        run.text = missionary.surname
+
+    # Insert Missionary ID
+    miss_id_holder = title_slide.placeholders[11]
+    assert miss_id_holder.has_text_frame
+    miss_id_holder.text_frame.clear()
+    p = miss_id_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    run.text = "Missionary ID: " + missionary.id
+
+    # Insert current year
+    miss_id_holder = title_slide.placeholders[1]
+    assert miss_id_holder.has_text_frame
+    miss_id_holder.text_frame.clear()
+    p = miss_id_holder.text_frame.paragraphs[0]
+    run = p.add_run()
+    run.text = "Reports - " + str(datetime.now().year)
+
+    return prs
+
+
+def create_powerpoint(missionary):
+    # Import presentation
+    path = "C:\Users\\br1\Dropbox\NCM\Reports\Ben Report Automation\\"
+    prs = Presentation(path + "Master Report Template.pptx")
+
+    # Title slide - requires name and ID only.
+    create_title_slide(prs, missionary)
+
+    counter = 1
+    for report_no, report in sorted(sorted(missionary.reports.iteritems(),
+                                           key=itemgetter(0),
+                                           reverse=True),
+                                    key=itemgetter(1),
+                                    reverse=True):
+        for report_split in report.report:
+            build_report_slide(prs, missionary, report, report_split)
+            counter += 1
+
+    # TODO - Save the powerpoint in a folder with Missionary ID
+    path = "C:\Users\\br1\Code\\500k\\reports\\{0}_{1}.pptx".format(
+        missionary.id,
+        missionary.surname)
+    prs.save(path)
+    logging.info("Reports for {0} have been saved to {1}.".format(
+                    missionary.id, path))
+    return path
+
+
+def create_powerpoint_pdf(q):
+    while True:
+        try:
+            (missionary, miss_id) = q.get()
+            logging.info("Building pptx file for {0}".format(miss_id))
+            path = create_powerpoint(missionary)
+
+            pythoncom.CoInitialize()
+            # Export to pdf - this is the slowest part so thread.
+            if path:
+                try:
+                    PPTtoPDF(path, path.split(".")[0] + ".pdf")
+                    try:
+                        os.remove(path)
+                    except OSError:
+                        logging.warning("Could not find {0} to delete".format(
+                            path))
+                except:
+                    logging.error(
+                        "Build PDF failed for missionary with ID: {0}".format(
+                            miss_id))
+            else:
+                logging.error(
+                    "Missing pptx for missionary with ID: {0}".format(
+                        miss_id))
+        except:
+            logging.error("{0} has died!".format(
+                          threading.current_thread().name))
+        finally:
+            q.task_done()
